4cbac5fcca349c3d5ee18faadc4ca2ea349087d2
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-05-27 01:48:36 -0600

    Table design, excel to table

---
+++ src/app/components/dropzone.tsx
@@ -0,0 +1,94 @@
+import { TrashIcon, UploadIcon } from "../../../public/icons/icons"
+import React, { useState, ChangeEvent } from 'react'
+import { useFileStore } from "../store/fileStore"
+
+export function Dropzone() {
+  const file = useFileStore((state) => state.file)
+  const setFile = useFileStore((state) => state.setFile)
+
+
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+  
+  const handleDragOver = (e:  React.DragEvent<HTMLLabelElement>) => {
+    e.preventDefault()
+    setIsDragging(true)
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLLabelElement>) => {
+    e.preventDefault()
+    setIsDragging(false)
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
+    e.preventDefault()
+    const extension = e.dataTransfer?.files[0].name
+    if (extension.endsWith('.xlsx')) {
+      const droppedFile = e.dataTransfer?.files[0]
+      if (droppedFile) {
+        setFile(droppedFile)
+      }
+    }
+    setIsDragging(false)
+  }
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const selectedFile= e.target.files?.[0]
+    if (selectedFile !== undefined) {
+      setFile(selectedFile)
+    }
+  }
+
+  const handleRemoveFile = () => {
+    setFile(null)
+  }
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 m-4">
+        <div className="flex w-[500px] justify-between items-end">
+          <p className='font-bold text-sm'>Archivo</p>
+          {
+            file && <p className='text-[10px] text-black/60'>{(file?.size / 1024).toFixed(2)} KB</p>
+          }
+        </div>
+        <div className="flex justify-center w-[500px] p-[6px] rounded-lg bg-white drop-shadow-md">
+        {
+          file ?
+          <>
+          <div className="flex flex-row gap-1 justify-between items-center w-[500px] h-[45px] px-4 py-2 bg-blue-100 text-black text-sm rounded-lg border-blue-500 border-solid border-[1px]">
+            <p>
+              <span className='font-bold'>{file.name}</span><span className='font-light'> cargado correctamente</span>
+            </p>
+            <TrashIcon className='cursor-pointer' onClick={handleRemoveFile}/>
+          </div>
+          </>
+            :
+          <>
+            <label htmlFor="file-upload" className={`flex flex-row gap-4 bg-gray-100 justify-center items-center w-[500px] h-[45px] text-black text-sm cursor-pointer rounded-lg border-gray-400 border-dashed border-[1px] transition-all hover:bg-blue-100 hover:border-blue-500 ${isDragging ? 'bg-slate-200 border-blue-500 border-solid' : ''}`}
+            onDragOver={(e) => handleDragOver(e)}
+            onDragLeave={(e) => handleDragLeave(e)}
+            onDrop={(e) => handleDrop(e)}
+            >  
+            {
+              isDragging ?
+              <p className='font-bold pointer-events-none'>¡Suelta aquí!</p>
+              :
+              <>
+                <span>
+                  <UploadIcon/>
+                </span>
+                <span className='font-light'>
+                  Arrastra el archivo .xlsx o <span className='font-bold'>Click para buscar</span>
+                </span>
+              </>
+            }
+          </label>
+          <input type="file" accept='.xlsx' id='file-upload' className='hidden' onChange={(e) => handleFileChange(e)}/>
+        </>
+      }
+        </div>
+      </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
 'use client'
-import * as xlsx from 'xlsx'
-import { TrashIcon, UploadIcon } from '../../public/icons/icons'
-import React, { useState, ChangeEvent } from 'react'
+import * as XLSX from 'xlsx';
+import React, { useEffect, useState } from 'react'
+import { Dropzone } from './components/dropzone'
+import { useFileStore } from './store/fileStore'
 
-export function Dropzone() {
-  const [file, setFile] = useState<File | null>(null)
-  const [isDragging, setIsDragging] = useState<boolean>(false)
-  
-  const handleDragOver = (e:  React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault()
-    setIsDragging(true)
-  }
+// interface Participant {
+//   id: number,
+//   name: string,
+//   strength: number,
+//   speed: number,
+//   score: number,
+//   time: string,
+//   total: number
+// }
 
-  const handleDragLeave = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault()
-    setIsDragging(false)
-  }
+type ExcelData = (string | number | boolean | null)[][] | null;
 
-  const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault()
-    const extension = e.dataTransfer?.files[0].name
-    if (extension.endsWith('.xlsx')) {
-      const droppedFile = e.dataTransfer?.files[0]
-      if (droppedFile) {
-        setFile(droppedFile)
+
+export function Table() {
+  const file = useFileStore((state) => state.file)
+  const [excelData, setExcelData] = useState<ExcelData>(null)
+
+  useEffect(() => {
+    if (!file) {
+      setExcelData(null)
+      return
+    }
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const result = e.target?.result
+      if (result instanceof ArrayBuffer) {
+        const data = new Uint8Array(result)
+        const workbook = XLSX.read(data, { type: 'array' })      
+        const sheetName = workbook.SheetNames[0]
+        const sheet = workbook.Sheets[sheetName]
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 }) as ExcelData
+        setExcelData(jsonData)        
       }
     }
-    setIsDragging(false)
-  }
+    reader.readAsArrayBuffer(file)
+  }, [file])
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const selectedFile= e.target.files?.[0]
-    if (selectedFile !== undefined) {
-      setFile(selectedFile)
-    }
-  }
+  
 
-  const handleRemoveFile = () => {
-    setFile(null)
-  }
   return (
     <>
-      <div className="flex flex-col gap-2 m-4">
-        <div className="flex w-[500px] justify-between items-end">
-          <p className='font-bold text-sm'>Subir el archivo</p>
-          {
-            file && <p className='text-[10px] text-black/60'>{(file?.size / 1024).toFixed(2)} KB</p>
-          }
-        </div>
-        <div className="flex justify-center w-[500px] p-[6px] rounded-lg bg-white drop-shadow-md">
-        {
-          file ?
-          <>
-          <div className="flex flex-row gap-1 justify-between items-center w-[500px] h-[45px] px-4 py-2 bg-blue-100 text-black text-sm rounded-lg border-blue-500 border-solid border-[1px]">
-            <p>
-              <span className='font-bold'>{file.name}</span><span className='font-light'> cargado correctamente</span>
-            </p>
-            <TrashIcon className='cursor-pointer' onClick={handleRemoveFile}/>
+      {
+        excelData ? 
+        <>
+          <div className="flex justify-start w-full">
+            <div className='table table-auto m-4 bg-slate-50 w-[1200px] rounded-md border-solid border-[1px] border-black/20 overflow-hidden'>
+              <div className='table-header-group bg-slate-100/80'>
+                <div className='table-row'>
+                  <div className='table-cell py-3'></div>
+                  {excelData[0].map((cell, index) => (
+                    <div key={index} className='table-cell py-3 px-3 text-sm text-left font-semibold'>{cell}</div>
+                  ))}
+                  <div className='table-cell py-3'></div>
+                </div>
+              </div>
+              <div className='table-row-group'>
+                {excelData.slice(1).map((row, rowIndex) => (
+                  <div key={rowIndex} className='table-row hover:bg-gray-200'>
+                    <div className="table-cell text-center pl-2 py-[6px] text-sm border-solid border-t-[1px] border-black/20"><input type="checkbox" className='appearance-none align-middle  w-4 h-4 rounded-full border-solid border-[1px] border-black/40 checked:bg-blue-700'/></div>
+                    {row.map((cell, cellIndex) => (
+                      <>
+                        <div key={cellIndex} className='table-cell px-4 py-[6px] text-sm border-solid border-t-[1px] border-black/20'>{cell}</div>
+                      </>
+                    ))}
+                    <div className='table-cell text-blue-700 text-sm font-medium border-solid border-t-[1px] border-black/20'><button className='w-full'>Editar</button></div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          </>
-            :
-          <>
-            <label htmlFor="file-upload" className={`flex flex-row gap-4 bg-gray-100 justify-center items-center w-[500px] h-[45px] text-black text-sm cursor-pointer rounded-lg border-gray-400 border-dashed border-[1px] transition-all hover:bg-blue-100 hover:border-blue-500 ${isDragging ? 'bg-slate-200 border-blue-500 border-solid' : ''}`}
-            onDragOver={(e) => handleDragOver(e)}
-            onDragLeave={(e) => handleDragLeave(e)}
-            onDrop={(e) => handleDrop(e)}
-            >  
-            {
-              isDragging ?
-              <p className='font-bold pointer-events-none'>¡Suelta aquí!</p>
-              :
-              <>
-                <span>
-                  <UploadIcon/>
-                </span>
-                <span className='font-light'>
-                  Arrastra el archivo .xlsx o <span className='font-bold'>Click para buscar</span>
-                </span>
-              </>
+        </>
+        : 
+        <p>Inserta el excel</p>
 
       }
-            </label>
-            <input type="file" accept='.xlsx' id='file-upload' className='hidden' onChange={(e) => handleFileChange(e)}/>
-          </>
-        }
-        </div>
-      </div>
-          </>
-        )
-      }
+    </>
+  )
+}
 
 
 export default function Home() {
   return (
     <>
       <Dropzone />
+      <Table />
     </>
   )
 }
