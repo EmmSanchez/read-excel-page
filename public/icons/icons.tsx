85795082bce04e4948227ca39688d2754a3082bc
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-05-29 01:08:22 -0600

    Remove rows

--- public/icons/icons.tsx
+++ public/icons/icons.tsx
@@ -49,3 +49,29 @@ export const PrintIcon = (props: SVGProps<SVGSVGElement>) => (
     <path d="M640-640v-120H320v120h-80v-200h480v200h-80Zm-480 80h640-640Zm560 100q17 0 28.5-11.5T760-500q0-17-11.5-28.5T720-540q-17 0-28.5 11.5T680-500q0 17 11.5 28.5T720-460Zm-80 260v-160H320v160h320Zm80 80H240v-160H80v-240q0-51 35-85.5t85-34.5h560q51 0 85.5 34.5T880-520v240H720v160Zm80-240v-160q0-17-11.5-28.5T760-560H200q-17 0-28.5 11.5T160-520v160h80v-80h480v80h80Z" />
   </svg>
 )
+
+export const AddIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="#e8eaed"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+  </svg>
+)
+
+export const RemoveIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="#e8eaed"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path d="M200-440v-80h560v80H200Z" />
+  </svg>
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import * as XLSX from 'xlsx';
-import React, { useEffect, useState } from 'react'
-import { Dropzone } from './components/dropzone'
-import { useFileStore } from './store/fileStore'
-import { EditIcon, PrintIcon, TrashIcon } from '../../public/icons/icons';
+import React, { useEffect, useState } from 'react';
+import { Dropzone } from './components/dropzone';
+import { useFileStore } from './store/fileStore';
+import { AddIcon, EditIcon, PrintIcon, RemoveIcon, TrashIcon } from '../../public/icons/icons';
 
 // interface Participant {
 //   id: number,
@@ -17,46 +17,105 @@ import { EditIcon, PrintIcon, TrashIcon } from '../../public/icons/icons';
 
 type ExcelData = (string | number | boolean | null)[][] | null;
 
-
 export function Table() {
-  const file = useFileStore((state) => state.file)
-  const [excelData, setExcelData] = useState<ExcelData>(null)
+  const file = useFileStore((state) => state.file);
+  const [excelData, setExcelData] = useState<ExcelData>(null);
+  const [rowToDelete, setRowToDelete] = useState<number | null>(null);
+  const [selectedRows, setSelectedRows] = useState<number[]>([])
 
+  // CONVERT EXCEL TO JSON
   useEffect(() => {
     if (!file) {
-      setExcelData(null)
-      return
+      setExcelData(null);
+      return;
     }
 
-    const reader = new FileReader()
+    const reader = new FileReader();
     reader.onload = (e) => {
-      const result = e.target?.result
+      const result = e.target?.result;
       if (result instanceof ArrayBuffer) {
-        const data = new Uint8Array(result)
-        const workbook = XLSX.read(data, { type: 'array' })      
-        const sheetName = workbook.SheetNames[0]
-        const sheet = workbook.Sheets[sheetName]
-        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 }) as ExcelData
-        setExcelData(jsonData)        
+        const data = new Uint8Array(result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 }) as ExcelData;
+        setExcelData(jsonData);
       }
-    }
-    reader.readAsArrayBuffer(file)
-  }, [file])
+    };
+    reader.readAsArrayBuffer(file);
+    setSelectedRows([])
+  }, [file]);
 
-  const handleDeleteRow = (rowIndex: number) => {
-    const id = rowIndex + 1    
-    const newExcelData = excelData?.filter((e, index) => index !== id) as ExcelData
-    setExcelData(newExcelData)
+  const handleGetRow = (rowIndex: number, action: string) => {
+    if(!action) return
+
+    
+    if (action === 'select') {
+      setSelectedRows(prev => {
+        if (prev?.includes(rowIndex)) {
+          return prev.filter(index => index !== rowIndex); // Deselect if already selected
+        } else {
+          return [...prev, rowIndex]; // Select if not already selected
+        }
+      });
+    }
+
+    if(action === 'delete') setRowToDelete(rowIndex)
+  };
+
+  // DELETE ROW
+  const confirmDeleteRow = () => {
+    if (rowToDelete !== null) {
+      const newExcelData = excelData?.filter((_, index) => index !== rowToDelete + 1) as ExcelData;
+      setExcelData(newExcelData);
+      setRowToDelete(null);
+      setSelectedRows([])
+    }
+  };
+  const cancelDelete = () => {
+    setRowToDelete(null);
+  };
+  // --------
+
+  // DELETE MORE THAN 2 ROWS
+  const deleteSelectedRows = () => {
+    if(!selectedRows || !excelData) return
+    const newData = excelData.filter((_, index) => !selectedRows.includes(index - 1)) as ExcelData
+    setExcelData(newData)
+    setSelectedRows([])
   }
-  
+
+  // ADD NEW ROW
+  const handleNewRow = () => {
+
+  }
+
+  // TO DO LIST
+  // - Popover for remove rows
+  // - Add rows
+  // - Edit rows
+  // - Search for ID
 
   return (
     <>
-      {
-        excelData ? 
+      {excelData ? (
         <>
-          <div className="flex justify-start w-full">
-            <div className='table table-auto m-4 bg-slate-50 w-[1200px] rounded-md border-solid border-[1px] border-black/20 overflow-hidden'>
+          <div className="flex flex-col justify-start w-full">
+            <div className="flex mx-3 pr-2 w-[1200px] justify-end gap-3 text-sm">
+              <button disabled={selectedRows.length > 0} onClick={handleNewRow} className={`px-4 py-[6px] rounded-md text-[#2563EB] font-semibold border-[1.4px] border-solid border-[#E2E8F0] transition-all ${selectedRows.length > 0 ? 'text-[#E2E8F0] cursor-not-allowed' : ''}`}>
+                <div className="flex justify-center items-center gap-1">
+                  <AddIcon fill={`${selectedRows.length > 0 ? '#E2E8F0' : '#2563EB'}`} width={18} height={18}/> 
+                  <p>Añadir</p>
+                </div>
+              </button>
+              <button disabled={selectedRows.length === 0} onClick={() => deleteSelectedRows()} className={`px-4 py-[6px] rounded-md text-[#2563EB] font-semibold border-[1.4px] border-solid border-[#E2E8F0] transition-all ${selectedRows.length === 0 ? 'text-[#E2E8F0] cursor-not-allowed' : ''}`}>
+                <div className="flex justify-center items-start gap-1">
+                  <RemoveIcon fill={`${selectedRows.length === 0 ? '#E2E8F0' : '#2563EB'}`} width={18} height={18}/>
+                  <p>Eliminar</p>
+                </div> 
+              </button>
+            </div>
+            <div className='table table-auto m-3 bg-slate-50 w-[1200px] rounded-md border-solid border-[1px] border-black/20 overflow-hidden'>
               <div className='table-header-group bg-slate-200/90'>
                 <div className='table-row'>
                   <div className='table-cell py-3'></div>
@@ -68,14 +127,18 @@ export function Table() {
               </div>
               <div className='table-row-group'>
                 {excelData.slice(1).map((row, rowIndex) => (
-                  <div key={rowIndex} className='table-row hover:bg-gray-200'>
+                  <div key={rowIndex} onClick={(e) => {e.stopPropagation(); handleGetRow(rowIndex, 'select')}} className='table-row hover:bg-gray-200'>
                     <div className="table-cell align-middle pl-1 py-[6px] text-center text-sm border-solid border-t-[1px] border-black/20">
-                      <input type="checkbox" className="appearance-none w-4 h-4 align-middle border-solid border-[1.4px] border-zinc-500 rounded-sm cursor-pointer checked:bg-blue-600 checked:border-blue-600 checked:bg-[url('../../public/icons/checkIcon.svg')] checked:bg-center checked:bg-cover"/>
+                      <input type="checkbox" 
+                        checked={selectedRows.includes(rowIndex)} 
+                        onClick={(e) =>{ 
+                            e.stopPropagation()
+                            handleGetRow(rowIndex, 'select');
+                          }} 
+                        className="appearance-none w-4 h-4 align-middle border-solid border-[1.4px] border-zinc-500 rounded-sm cursor-pointer checked:bg-blue-600 checked:border-blue-600 checked:bg-[url('../../public/icons/checkIcon.svg')] checked:bg-center checked:bg-cover"/>
                     </div>
                     {row.map((cell, cellIndex) => (
-                      <>
-                        <div key={cellIndex} className='table-cell align-middle px-4 py-[6px] text-sm border-solid border-t-[1px] border-black/20'>{cell}</div>
-                      </>
+                      <div key={cellIndex} className='table-cell align-middle px-4 py-[6px] text-sm border-solid border-t-[1px] border-black/20'>{cell}</div>
                     ))}
                     <div className='table-cell align-middle py-[6px] border-solid border-t-[1px] border-black/20'>
                       <div className="flex gap-[2px] pr-1">
@@ -85,13 +148,21 @@ export function Table() {
                         <button className='w-full h-full'>
                           <EditIcon className='bg-slate-800 p-1 rounded-md active:bg-slate-950 transition-all'/>
                         </button>
-                        <button popoverTarget='delete-popover' className='w-full h-full' onClick={(e) => handleDeleteRow(rowIndex)}>
+                        <button className='w-full h-full' onClick={(e) => {e.stopPropagation(); handleGetRow(rowIndex, 'delete')}}>
                           <TrashIcon width={24} height={24} fill='#e8eaed' className='bg-red-800 p-1 rounded-md active:bg-red-950 transition-all'/>
-                          <div id='delete-popover' popover='auto'>
-                            <p>¿Estás seguro de que deseas eliminar este elemento?</p>
-                            <p>Eliminar usuario</p>
-                          </div>
                         </button>
+                        {rowToDelete === rowIndex && (
+                          <div id='delete-popover' className='fixed w-[30%] right-5 bottom-10 bg-zinc-50 shadow-lg rounded-md p-4 text-xs'>
+                            <div className="flex flex-col gap-2">
+                              <p>¿Estás seguro de que deseas eliminar este elemento?</p>
+                                <hr />
+                              <div className="flex justify-end gap-4">
+                                <button onClick={(e) => {e.stopPropagation(); cancelDelete()}} className="p-2 rounded-md hover:bg-gray-200">Cancelar</button>
+                                <button onClick={(e) => {e.stopPropagation(); confirmDeleteRow()}} className="bg-red-600 text-white p-2 rounded-md hover:bg-red-800">Eliminar</button>
+                              </div>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
@@ -100,14 +171,12 @@ export function Table() {
             </div>
           </div>
         </>
-        : 
+      ) : (
         <p>Inserta el excel</p>
-
-      }
+      )}
     </>
-  )
+  );
 }
-
 
 export default function Home() {
   return (
@@ -115,5 +184,6 @@ export default function Home() {
       <Dropzone />
       <Table />
     </>
-  )
+  );
 }
+
